3b37b908204882317d8fa79fff9eaa9866b8e513
ashifujjmanRafi <ajrafi2722@gmail.com>
2021-03-24 20:06:20 +0600

    solving codeforces problem

---
+++ AvoidingZero.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution()
+{
+    int n;
+    cin >> n;
+    int arr[n + 1];
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    if (sum == 0)
+        cout << "No" << endl;
+
+    else
+    {
+        cout << "Yes" << endl;
+        sort(arr, arr + n);
+        if (sum > 0)
+        {
+            for (int i = n - 1; i >= 0; i--)
+            {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+int main()
+{
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        solution();
+    }
+
+    return 0;
+}
