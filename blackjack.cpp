6e0e5cf2dc03188b5c26a1729bbf5492600b6d4e
ashifujjmanRafi <ajrafi2722@gmail.com>
2021-04-06 13:33:19 +0600

    problem solving

---
+++ blackjack.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    //n = n - 10;
+    if (n < 10)
+        cout << 0 << endl;
+    else
+    {
+        n = n - 10;
+        if (n == 0 || n > 11)
+            cout << 0 << endl;
+        else if (n == 10)
+        {
+            cout << 15 << endl;
+        }
+        else
+            cout << 4 << endl;
+    }
+    return 0;
+}
