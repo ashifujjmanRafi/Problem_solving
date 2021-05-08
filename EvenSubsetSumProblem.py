10981e9428427e4891757709af2f2ca3731487aa
ashifujjmanRafi <ajrafi2722@gmail.com>
2021-05-08 17:07:58 +0600

    problem solving with python

--- EvenSubsetSumProblem.py
+++ EvenSubsetSumProblem.py
@@ -1,18 +1,22 @@
 for i in range(int(input())):
     index= int(input())
-    array=[int(itm) for itm in input().split()]
-    a,b=0,0
-    for i in range(len(array)):
-        if(array[i]%2==0):
-            print(i+1)
-            break
-        elif(a<3 and array[i]%2!=0):
-            b=i
-            array.append(b)
-            a+=1
+    #array=[int(itm) for itm in input().split()]
+    a,b,c=0,0,0
+    for int(input()) in range(index):
+        x=
+        if(x%2==0):
+            a=j+1
+        elif(x%2!=0):
+            b=j+1
+        elif(x%2!=0):
+            c=j+1
     
-    if(a==2):
-        print(array[index:])
+    if(a!=0):
+        print("1")
+        print(a)
+    elif(b!=0 and c!=0):
+        print("2")
+        print(b,c)
     else:
         print("-1")
             
