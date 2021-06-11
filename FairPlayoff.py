1fcdd88f2800274ad947bce6c29d07d1ea8beafa
ashifujjmanRafi <ajrafi2722@gmail.com>
2021-06-11 11:22:43 +0600

    problem solving with python

---
+++ FairPlayoff.py
@@ -0,0 +1,7 @@
+for i in range(int(input())):
+    a,b,c,d= map(int,input().split())
+    if(min(a,b)>max(c,d) or min(c,d)>max(a,b) ):
+        print("NO")
+    else:
+        print("YES")
+        
